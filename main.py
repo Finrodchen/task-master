7eb331dbacf9748c6a099af1db3f5a04d464a51a
Finrodchen <s02260441@gmail.com>
2020-06-03 21:58:10 +0800

    make it better

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app=Flask(__name__) #__name__目前執行的模組
+
+@app.route("/") #函式的裝飾
+def home():
+    return "Hello World! 2"
+
+@app.route("/test")
+def test():
+    return "This is test"
+
+
+if __name__=="__main__": #如果以上程式執行
+    app.run() #執行app
