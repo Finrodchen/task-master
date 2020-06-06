ba5aea1929e669ace72194f5c03eaf2641a09106
Finrodchen <s02260441@gmail.com>
2020-06-07 05:12:43 +0800

    major release

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app=Flask(__name__) #__name__目前執行的模組
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kanban.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    priority = db.Column(db.String(200), nullable = False)
     content = db.Column(db.String(200), nullable = False)
     people = db.Column(db.String(200), nullable = False)
     completed = db.Column(db.Integer, default = 0)
@@ -19,15 +20,17 @@ class Todo(db.Model):
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content =request.form['content']
-        new_task = Todo(content=task_content)
+        task_priority = request.form['priority']
+        task_content = request.form['content']
+        task_people = request.form['people']
+        new_task = Todo(priority=task_priority, content=task_content, people=task_people)
 
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
-            return "Adding new issue to your task."    
+            return "Fail to add new issue to your task."    
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
@@ -49,7 +52,9 @@ def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == "POST":
+        task.priority = request.form['priority']
         task.content = request.form['content']
+        task.people = request.form['people']
 
         try:
             db.session.commit()
